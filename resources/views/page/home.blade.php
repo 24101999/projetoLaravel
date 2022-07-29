e051dd49274fd5f6c8a883e0d94d092e052844b4
Henrique <henriquedasilvacosta@live.com>
2022-07-29 16:56:09 -0300

    em andamendto

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,3 +1,29 @@
 @extends('page.layout')
 
-<h1>test</h1>
+<header>
+<h1 class="h1-header">Henrique developer</h1>
+<h2 class="h2-blocks-header">tecnologias usadas nesse site</h2>
+<div class="blocks-header">
+    <div class="block-header"><h2>PHP</h2></div>
+    <div class="block-header"><h2>Laravel</h2></div>
+    <div class="block-header"><h2>JavaScript</h2></div>
+    <div class="block-header"><h2>Sql</h2></div>
+</div>
+</header>
+
+<article class="responsive">
+<div class="responsive-imgs">
+<img class="img-responsive" src="/imagens/header.jpg" alt="" height="300px">
+<img class="img-responsive" src="/imagens/header.jpg" alt="" height="300px">
+</div>
+<div class="responsive-imgs">
+<img class="img-responsive" src="/imagens/header.jpg" alt="" height="300px">
+<img class="img-responsive" src="/imagens/header.jpg" alt="" height="300px">
+</div>
+</article>
+
+<footer>
+    
+</footer>
+
+
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/home.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css')
-    .sass('resources/css/home.sass', 'public/css')
+    .css('resources/css/home.css', 'public/css')
